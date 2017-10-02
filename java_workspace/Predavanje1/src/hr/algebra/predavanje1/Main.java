d23939e791f142a3ed7ac52b2d517884970a899f
Stargatzer <32370329+Stargatzer@users.noreply.github.com>
2017-10-02 19:15:05 +0200

    Add files via upload

---
+++ java_workspace/Predavanje1/src/hr/algebra/predavanje1/Main.java
@@ -0,0 +1,18 @@
+package hr.algebra.predavanje1;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		// Slijedi prvi testni ispis
+		int a = 8;
+		int b = 3;
+		double rez = (double)a/b;
+		
+		System.out.println("Rez = " + rez);
+
+		
+		
+	}
+
+}
---
+++ java_workspace/Predavanje2/src/hr/algebra/predavanje2/Main.java
@@ -0,0 +1,41 @@
+package hr.algebra.predavanje2;
+
+import java.util.Scanner; //Scanner je velikim poèetnim pa je klasa 
+
+public class Main {
+	
+/*
+ * 
+ * alt 91 [
+ * alt 93 ]
+ * alt+64 = @
+ * 123 }
+ * 125 [
+ * ctrl+shift+f format code
+ */
+	
+	public static void main(String[] args) {
+		
+		
+		// matematièke funkcije i konstante
+		
+		
+		/*
+		 * // round & E 
+		 * double mojBroj = 2.1239; System.out.println("E = " +
+		 *Math.round(mojBroj * 1000) / 1000d); // metoda pow mojBroj = 40;
+		 * double potencija = 2; System.out.println("40^2 = " +
+		 * Math.pow(mojBroj, potencija));
+		 */
+		
+		// unesite broj i izraèunajte njegov kvadrat (scanner) !
+		Scanner scanner = new Scanner(System.in);//novi objekt pomoæu konstruktora klase Scanner
+		System.out.println("Unesite broj");
+		int uneseniBroj = scanner.nextInt();
+		System.out.println("unesen je broj "+uneseniBroj+" kvadrat broja = "+Math.pow(uneseniBroj, 2));
+		
+		
+		
+	}
+
+}
